9a86ecb9905cde2d6784310ac9d3a5e387d70f59
omarfaysal1111 <omarfaysal44@gmail.com>
2026-06-14 19:55:48 +0300

    feat: implement login screen with email and password fields

--- lib/features/auth/presentation/pages/login_screen.dart
+++ lib/features/auth/presentation/pages/login_screen.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    // TODO: implement
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Column(
+        children: [
+TextFormField(controller: emailController, decoration: const InputDecoration(labelText: 'Email')),
+
+TextFormField(controller: passwordController, decoration: const InputDecoration(labelText: 'Password')),
+          MaterialButton(onPressed: (){},child: Text("Login"),),
+          
+        ],
+      )
+      
+    );
   }
+
+  
 }
